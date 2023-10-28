2774c5e939f91a2cccf2626f87db7777f04e7584
Ramin RX7 <rawmin.rx@gmail.com>
2023-10-28 11:21:08 +0330

    feat: add main.py

---
+++ src/main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+
+from config import SETTINGS
+
+from api import router
+
+
+
+
+app = FastAPI()
+
+app.include_router(router)
+
+@app.get("/")
+async def index():
+    return {}
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("__main__:app", host="0.0.0.0", port=8001, reload=True)
