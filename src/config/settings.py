e7c67a70ff671b9a096811bcf6e19617938c3edf
Ramin RX7 <rawmin.rx@gmail.com>
2023-10-31 01:25:14 +0330

    config: update settings

--- src/config/settings.py
+++ src/config/settings.py
@@ -1,17 +1,20 @@
-import os
 from pydantic_settings import BaseSettings
 
 
 
-class _Config:#(BaseSettings):
-    MONGODB_URL : str = "mongodb://mongo:27017"
 
-    REDIS_URL : str = "redis://redis:6379"
-    REDIS_KEY_TTL : int = 60*60
+class _Config(BaseSettings):
+    MONGODB_URL : str
+
+    REDIS_URL : str
+    REDIS_KEY_TTL : int
+
+    # REDIX : _RedisConfig
 
     class Config:
-        env_file = ".env"
+        # env_file = ".env"
         extra = "ignore"
+        env_nested_delimiter = "_"
 
 
 SETTINGS = _Config()
