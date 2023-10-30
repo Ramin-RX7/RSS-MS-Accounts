165d3f2752857d0d5cc27b83400c39e84b3f5b17
Ramin RX7 <rawmin.rx@gmail.com>
2023-10-31 01:26:41 +0330

    style: split auth.validators lines of return

--- src/auth/validators.py
+++ src/auth/validators.py
@@ -17,14 +17,15 @@ def _password_characters_validator(password):
             if char in char_group:
                 condition_dict[char_group] = True
                 break
-    if not all(condition_dict.values()):
-        raise ValidationError(
-            "Password must contain one lower case, one upper case, one digit and one punctuation"
-        )
+    assert all(condition_dict.values()), (
+        "Password must contain at least one lower case, upper case, digit, punctuation"
+    )
 def password_validator(raw_password:str):
     return _password_length_validator(raw_password) # and _password_characters_validator(raw_password)
 
 
 def username_validator(username:str):
     assert len(username) >= 5, "Username must be at least 5 characters"
-    assert re.fullmatch(r"\w+", username), "Username must contain only letters, digits and unserscore"
+    assert re.fullmatch(r"\w+", username), (
+        "Username must contain only letters, digits and unserscore"
+    )
