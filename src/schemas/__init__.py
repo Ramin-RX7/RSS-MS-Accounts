c7d050faedcda952e6a6150b1bae9b0bf8ccc646
Ramin RX7 <rawmin.rx@gmail.com>
2023-10-31 01:29:29 +0330

    feat: add ChangePassword Scheme

--- src/schemas/__init__.py
+++ src/schemas/__init__.py
@@ -1,9 +1,10 @@
 from datetime import datetime
 
-from pydantic import BaseModel,Field
+from pydantic import BaseModel, model_validator,Field
 
 from db import db
 from .base import *
+from auth.validators import password_validator
 
 collection = db["accounts"]
 
@@ -26,8 +27,18 @@ class Profile(BaseModel):
     birth_date: datetime|None = None
 
 
-class User(Profile, BaseModel):
-    # _id : ObjectId|None
+class ChangePassword(BaseModel):
+    old_password : str
+    new_password : str
+    confirm_password : str
+
+    @model_validator(mode='after')
+    def check_passwords_match(self):
+        pw1 = self.new_password
+        pw2 = self.confirm_password
+        password_validator(pw1)
+        assert pw1==pw2, "Password and confirm password are not equal"
+        return self
     username: str
     email: str
     # password: str   #? Commented out so it won't be retrieved from database (retrieved but not saved)
