c31a4d0a4bf9b9e260278bc56fe77fa0d047f6cc
Ramin RX7 <rawmin.rx@gmail.com>
2023-10-30 12:48:19 +0330

    feat: add RedisService

--- src/config/settings.py
+++ src/config/settings.py
@@ -3,8 +3,11 @@ from pydantic_settings import BaseSettings
 
 
 
-class _Config(BaseSettings):
-    MONGODB_URL : str = os.environ.get("MONGODB_URL")
+class _Config:#(BaseSettings):
+    MONGODB_URL : str = "mongodb://mongo:27017"
+
+    REDIS_URL : str = "redis://redis:6379"
+    REDIS_KEY_TTL : int = 60*60
 
     class Config:
         env_file = ".env"
