deb8885a05f175afafd02007c317026258b40188
Ramin RX7 <rawmin.rx@gmail.com>
2023-11-01 02:12:58 +0330

    chore: api.v1 based on new JWT changes

--- src/api/v1/__init__.py
+++ src/api/v1/__init__.py
@@ -2,17 +2,18 @@ from fastapi import APIRouter,Depends
 from fastapi.responses import JSONResponse
 
 from db import db
-from db.auth import add_user,check_password,delete_user, update_password,update_user
+from db.auth import add_user,check_password,delete_user,update_password,update_user
 from db.auth.errors import InvalidCredentials,UserAlreadyExists
-from schemas import Signup,Login,User,Result,Profile
-from auth.jwt_auth import JWTAuth
+from db.auth.utils import hash_password
+from schemas import ChangePassword, Email, Signup,Login,User,Result,Profile
+from auth.jwt_auth import JWTHandler
 
 
 router = APIRouter(prefix='/v1')
 
 ACCOUNTS_COLLECTION_NAME = "accounts"
 collection = db[ACCOUNTS_COLLECTION_NAME]
-jwt_object = JWTAuth()
+jwt_object = JWTHandler()
 
 
 
@@ -36,27 +37,32 @@ async def signup(user_data:Signup):
 
 @router.post('/login/', status_code=200)
 async def login(data:Login):
-    if not await check_password(data.username, data.password):
+    user = await collection.find_one({
+        "username":data.username,
+        "password":hash_password(data.password)
+    })
+    if not user:
         return JSONResponse(
             Result.resolve_error(InvalidCredentials).model_dump(),
             status_code=403
         )
-    return Result().model_dump()
+    return Result(user=User(**user)).model_dump()
+
 
 
 @router.get("/profile/")
-async def profile(user:User=Depends(jwt_object)):
+async def profile(user:User=Depends(jwt_object.get_user)):
     return user
 
 
 @router.put("/profile/",)
-async def profile_update(new_data:Profile, user:User=Depends(jwt_object)):
+async def profile_update(new_data:Profile, user:User=Depends(jwt_object.get_user)):
     user = user.model_copy(update=new_data.model_dump())
     await update_user(user.username, user)
     return Result()
 
 
 @router.delete("/profile/")
-async def profile_delete(user:User=Depends(jwt_object)):
+async def profile_delete(user:User=Depends(jwt_object.get_user)):
     await delete_user(user.username)
     return {}
