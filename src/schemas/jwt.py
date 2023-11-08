d0c4aabf62db12667ca8297b96b1947cbf665acc
Ramin RX7 <rawmin.rx@gmail.com>
2023-11-08 15:55:24 +0330

    fix: email -> id in JWTPayload

--- src/schemas/jwt.py
+++ src/schemas/jwt.py
@@ -1,9 +1,11 @@
+from bson import ObjectId
 from pydantic import BaseModel
 
 
 class JWTPayload(BaseModel):
-    email : str
+    id : ObjectId
     payload : dict
 
     class Config:
+        arbitrary_types_allowed = True
         extra = "ignore"
